4cb4d0a7c1843092c3aa95a7d96f1239c974a09b
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-05-25 01:13:48 -0700

    cors

--- src/http/get-graphql/index.js
+++ src/http/get-graphql/index.js
@@ -1,11 +1,9 @@
-let arc = require("@architect/functions");
-let { ApolloServer, gql } = require("apollo-server-lambda"); // TODO: we just need 'graphql-playground' here // not apollo
-
-let typeDefs = gql`
+const { ApolloServer, gql } = require("apollo-server-lambda"); // TODO: we just need 'graphql-playground' here // not apollo
+const typeDefs = gql`
   type Query {
     test: String
   }
 `;
-
-let server = new ApolloServer({ typeDefs, resolvers: {} });
+const resolvers = {};
+const server = new ApolloServer({ typeDefs, resolvers });
 exports.handler = server.createHandler();
